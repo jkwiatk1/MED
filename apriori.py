0271a28ee19f6399c9776503555783c106223efe
jkwiatk1 <01142696@pw.edu.pl>
2023-06-13 14:54:49 +0200

    Some changes

--- apriori.py
+++ apriori.py
@@ -1,14 +1,26 @@
 import pandas as pd
 import itertools
 
+########################################
+# Step 0: Parameters
+########################################
 IS_PRINT_ = False
-min_support = 120
+min_support = 100
+min_confidence = 0.2
 
+
+########################################
+# Step 1: Load data
+########################################
 df_transaction_dataset = pd.read_csv('transactions_with_parents.csv')
 
 # ONLY FOR TESTING
 df_transaction_dataset = df_transaction_dataset.head(3000)
 
+
+########################################
+# Step 2: Frequency Counting
+########################################
 # Count the occurrences of each individual item (4-digit columns)
 item_counts = df_transaction_dataset[[col for col in df_transaction_dataset.columns if col.isdigit() and len(col) == 4]].sum()
 
@@ -38,6 +50,9 @@ for index, row in df_transaction_dataset.iterrows():
                     unique_combinations.add(combination_key)
                     combination_counts[combination_key] = 1
 
+# Merge occuracy from each individual item with combinations for each hierarchical level
+combination_counts_series = pd.Series(combination_counts)
+transations_taxonomy_elements_frequency = pd.concat([item_counts, combination_counts_series])
 
 if IS_PRINT_ == True:
     # Print the items counts
@@ -46,30 +61,14 @@ if IS_PRINT_ == True:
     for combination, count in combination_counts.items():
         print(f"Combination: {combination}, Count: {count}")
 
-
-
-# Merge occuracy from each individual item with combinations for each hierarchical level
-combination_counts_series = pd.Series(combination_counts)
-transations_taxonomy_elements_frequency = pd.concat([item_counts, combination_counts_series])
-# # Convert the merged Series to a DataFrame
-# merged_df = pd.DataFrame({'Count': transations_taxonomy_elements_frequency})
-#
-# # Reset index of the merged DataFrame to make the columns accessible
-# merged_df.reset_index(inplace=True)
-# merged_df.rename(columns={'index': 'Item'}, inplace=True)
-
 if IS_PRINT_ == True:
     print(transations_taxonomy_elements_frequency)
 
 
-
+########################################
+# Step 3: Generate Frequent k-itemsets
+########################################
 # Function to generate candidate itemsets of length k
-'''
-mozna zamiast dwoch petli uzyc 
-import itertools
-for combination in itertools.combinations(prev_itemsets, 2):
-    candidates2.add(frozenset(combination))
-'''
 def generate_candidates(prev_itemsets, k):
     candidates = set()
     for itemset1 in prev_itemsets:
@@ -77,8 +76,6 @@ def generate_candidates(prev_itemsets, k):
             if len(itemset1.union(itemset2)) == k:
                 candidates.add(itemset1.union(itemset2))
     return candidates
-
-
 
 # Function to prune infrequent itemsets
 def prune_itemsets(itemsets, min_support, rejected_itemsets):
@@ -101,9 +98,7 @@ def prune_itemsets(itemsets, min_support, rejected_itemsets):
             rejected_itemsets.add(itemset)
     return pruned_itemsets
 
-########################################
-# Step 3: Generate Frequent k-itemsets
-########################################
+
 # Frequent itemsets dictionary
 frequent_itemsets = {}
 
@@ -119,7 +114,6 @@ for item, count in transations_taxonomy_elements_frequency.items():
 
 frequent_itemsets[1] = frequent_1_itemsets
 
-
 # Generate frequent k-itemsets
 k = 2
 while frequent_itemsets[k - 1]:
@@ -131,14 +125,12 @@ while frequent_itemsets[k - 1]:
 
     k += 1
 
-'''
-# Print frequent k-itemsets
-for k, itemsets in frequent_itemsets.items():
-    print(f"Frequent {k}-itemsets:")
-    for itemset, support in itemsets.items():
-        print(f"Itemset: {itemset}, Support: {support}")
-'''
-
+if IS_PRINT_ == True:
+    # Print frequent k-itemsets
+    for k, itemsets in frequent_itemsets.items():
+        print(f"Frequent {k}-itemsets:")
+        for itemset, support in itemsets.items():
+            print(f"Itemset: {itemset}, Support: {support}")
 
 
 ########################################
@@ -195,9 +187,6 @@ def generate_association_rules(frequent_itemsets_to_create_rules, min_confidence
 
     return association_rules
 
-
-
-
 # Function to get all possible subsets of an itemset
 def get_subsets(itemset):
     subsets = []
@@ -209,12 +198,8 @@ def get_subsets(itemset):
 
     return [frozenset(subset) for subset in subsets]
 
-
-# Set the minimum confidence threshold for association rules
-min_confidence = 0.3
-
 # Generate association rules from frequent itemsets: [k-2] - last;  [k-3] - one before last
-association_rules = generate_association_rules(frequent_itemsets[k-2], min_confidence)
+association_rules = generate_association_rules(frequent_itemsets[k-3], min_confidence)
 
 # Print the generated association rules
 print("Association Rules:")
